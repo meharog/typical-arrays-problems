624a276955d1401f7cba98c13ec908a5fad557c9
meharog <nklnkm@yandex.ru>
2020-03-21 08:47:29 +0300

    init: start Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+  if ((array === undefined) || (array.length === 0)) { return 0; };
+  let min = array[0];
+  if (array.length > 1) {
+    for (let i = 0; i < array.length; i++) {
+      if (min > array[i]) {
+        min = array[i];
+      };
+    };
+  };
+  return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if ((array === undefined) || (array.length === 0)) { return 0; };
+  let max = array[0];
+  if (array.length > 1) {
+    for (let i = 0; i < array.length; i++) {
+      if (max < array[i]) {
+        max = array[i];
+      };
+    };
+  };
+  return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if ((array === undefined) || (array.length === 0)) { return 0; };
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  };
+  return sum / array.length;
+};
